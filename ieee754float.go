51070f8a07bd9329c0ad8bd686a12157d4ac322d
koyachi <rtk2106@gmail.com>
2014-12-12 09:30:05 +0900

    add IEEE754Float80bit.SetLongValue() and test

--- ieee754float.go
+++ ieee754float.go
@@ -4,7 +4,7 @@ package aiff
 
 type IEEE754Float80bit [10]byte
 
-func (f IEEE754Float80bit) LongValue() uint32 {
+func (f *IEEE754Float80bit) LongValue() uint32 {
 	mantissa := uint32(f[2])<<24 | uint32(f[3])<<16 | uint32(f[4])<<8 | uint32(f[5])
 	exp := 30 - f[1]
 	var last uint32
@@ -16,4 +16,31 @@ func (f IEEE754Float80bit) LongValue() uint32 {
 		mantissa++
 	}
 	return mantissa
+}
+
+func (f *IEEE754Float80bit) SetLongValue(v uint32) {
+	var i uint32
+	for i = 0; i < 10; i++ {
+		f[i] = 0x00
+	}
+	exp := v
+	exp >>= 1
+	for i = 0; i < 32; i++ {
+		exp >>= 1
+		if exp == 0 {
+			break
+		}
+	}
+	f[0] = 0x40 // TODO
+	f[1] = byte(i)
+	for i = 32; i > 0; i-- {
+		if v&0x80000000 == 0x80000000 {
+			break
+		}
+		v <<= 1
+	}
+	f[2] = uint8((v & 0xFF000000) >> 24)
+	f[3] = uint8((v & 0x00FF0000) >> 16)
+	f[4] = uint8((v & 0x0000FF00) >> 8)
+	f[5] = uint8(v & 0x000000FF)
 }
--- ieee754float_test.go
+++ ieee754float_test.go
@@ -1,6 +1,7 @@
 package aiff
 
 import (
+	"bytes"
 	"testing"
 )
 
@@ -11,5 +12,15 @@ func TestIEEE754Float80bit(t *testing.T) {
 	f = data
 	if f.LongValue() != 44100 {
 		t.Fatalf("LongValue() is invalid: %d", f.LongValue())
+	}
+
+	var data2 [10]byte
+	data2 = [10]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
+	var f2 IEEE754Float80bit
+	f2 = data2
+	f2.SetLongValue(44100)
+	expected := [10]byte{0x40, 0x0e, 0xac, 0x44, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
+	if bytes.Compare(f2[:], expected[:]) != 0 {
+		t.Fatalf("SetLongValue(44100) is invalid: %v", f2)
 	}
 }
